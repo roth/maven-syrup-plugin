5cc421fc4634ae6533238c47c4c4caaadc8fbd67
dani.eichhorn@squix.ch <dani.eichhorn@squix.ch@5f25f452-1f30-fc56-cf57-e20835f732fe>
2012-04-12 22:29:35 +0000

    Added test harness

---
+++ maven-syrup-plugin/src/test/resources/com/netcetera/MyMojoTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2012 by Netcetera AG.
+ * All rights reserved.
+ *
+ * The copyright to the computer program(s) herein is the property of
+ * Netcetera AG, Switzerland.  The program(s) may be used and/or copied
+ * only with the written permission of Netcetera AG or in accordance
+ * with the terms and conditions stipulated in the agreement/contract
+ * under which the program(s) have been supplied.
+ */
+package com.netcetera;
+
+import java.io.File;
+
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+
+/**
+ * Example test for running a Mojo.
+ */
+public class MyMojoTest extends AbstractMojoTestCase {
+
+  /**
+   * @throws Exception if any
+   */
+  public void testSomething() throws Exception {
+    File pom = getTestFile("src/test/resources/pom.xml");
+    assertNotNull(pom);
+    assertTrue(pom.exists());
+
+    MyMojo myMojo = (MyMojo) lookupMojo("touch", pom);
+    assertNotNull(myMojo);
+    myMojo.execute();
+
+  }
+}
